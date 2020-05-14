c742b60de0de427fba74758675208a3c1410547d
Tijmen Wierenga <tijmen.wierenga@persgroep.net>
2020-05-14 21:31:16 +0200

    Created Token authenticator to authorize API calls

---
+++ src/Security/TokenAuthenticator.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Security;
+
+use ParagonIE\Paseto\Exception\PasetoException;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+final class TokenAuthenticator extends AbstractGuardAuthenticator
+{
+    private UserRepository $userRepository;
+    private TokenService $tokenService;
+
+    public function __construct(UserRepository $userRepository, TokenService $tokenService)
+    {
+        $this->userRepository = $userRepository;
+        $this->tokenService = $tokenService;
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null): Response
+    {
+        return new Response(null, Response::HTTP_FORBIDDEN);
+    }
+
+    public function supports(Request $request): bool
+    {
+        return $request->headers->has('Authorization');
+    }
+
+    public function getCredentials(Request $request): string
+    {
+        return $request->headers->get('Authorization');
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider): User
+    {
+        if (! preg_match('/^Bearer\s(.+)$/', $credentials, $matches)) {
+            throw new AuthenticationException('The Authorization header is not a valid Bearer token');
+        }
+
+        $rawToken = $matches[1];
+
+        try {
+            $jsonToken = $this->tokenService->parseToken($rawToken);
+            $userId = Uuid::fromString($jsonToken->getAudience());
+
+            return $this->userRepository->find($userId);
+        } catch (UserNotFoundException $e) {
+            throw new AuthenticationException('Could not find user based on token', 1, $e);
+        } catch (PasetoException $e) {
+            throw new AuthenticationException('Invalid token', 1, $e);
+        }
+    }
+
+    public function checkCredentials($credentials, UserInterface $user): bool
+    {
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        return new Response(null, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey): ?Response
+    {
+        return null;
+    }
+
+    public function supportsRememberMe(): bool
+    {
+        return false;
+    }
+}
---
+++ tests/Security/TokenAuthenticatorTest.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Security;
+
+use App\Security\TokenAuthenticator;
+use App\Security\TokenService;
+use App\Security\User;
+use App\Security\UserRepositoryInMemory;
+use Doctrine\Common\Collections\ArrayCollection;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+final class TokenAuthenticatorTest extends TestCase
+{
+    private TokenAuthenticator $authenticator;
+    private UserRepositoryInMemory $userRepository;
+    private TokenService $tokenService;
+    
+    protected function setUp(): void
+    {
+        $this->userRepository = new UserRepositoryInMemory();
+        $this->tokenService = new TokenService(random_bytes(32));
+        $this->authenticator = new TokenAuthenticator($this->userRepository, $this->tokenService);    
+    }
+
+    public function testItSupportsARequestWithAuthorizationHeader(): void
+    {
+        $request = Request::create('/issues');
+        $request->headers->add([
+            'Authorization' => 'Bearer some-token'
+        ]);
+
+        static::assertTrue($this->authenticator->supports($request));
+    }
+
+    public function testItReturnsA403ToIndicateAuthenticationIsNeeded(): void
+    {
+        $request = Request::create('/issues');
+        $response = $this->authenticator->start($request);
+
+        static::assertEquals(403, $response->getStatusCode());
+    }
+
+    public function testItExtractsTheTokenFromARequest(): void
+    {
+        $request = Request::create('/issues');
+        $request->headers->add([
+            'Authorization' => 'Bearer some-token'
+        ]);
+
+        static::assertEquals('Bearer some-token', $this->authenticator->getCredentials($request));
+    }
+
+    public function testItOnlyAcceptsABearerToken(): void
+    {
+        $this->expectException(AuthenticationException::class);
+
+        $this->authenticator->getUser('NonBearer token', $this->createMock(UserProviderInterface::class));
+    }
+
+    public function testItFailsAuthenticationWhenTokenIsInvalid(): void
+    {
+        $this->expectException(AuthenticationException::class);
+
+        $this->authenticator->getUser('Bearer fake-token', $this->createMock(UserProviderInterface::class));
+    }
+
+    public function testItAbortsAuthenticationWhenUserDoesNotExist(): void
+    {
+        $this->expectException(AuthenticationException::class);
+
+        $user = User::new(Uuid::uuid4(), 'a-user', 'a-password'); // User is not saved
+        $token = $this->tokenService->createToken($user);
+
+        $this->authenticator->getUser(sprintf('Bearer %s', $token), $this->createMock(UserProviderInterface::class));
+    }
+
+    public function testItReturnsTheUserFromAValidToken(): void
+    {
+        $user = User::new(Uuid::uuid4(), 'a-user', 'a-password');
+        $this->userRepository->users = new ArrayCollection([$user]); // Save the user
+        $token = $this->tokenService->createToken($user);
+
+        $result = $this->authenticator->getUser(sprintf('Bearer %s', $token), $this->createMock(UserProviderInterface::class));
+
+        static::assertEquals($user, $result);
+    }
+}

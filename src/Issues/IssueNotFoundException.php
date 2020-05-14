2844b180442260b5ea1fb6ca4a8d3214ecad8d2f
Tijmen Wierenga <tijmen.wierenga@persgroep.net>
2020-05-14 21:27:44 +0200

    Moved NotFoundException to shared namespace

--- src/Issues/IssueNotFoundException.php
+++ src/Issues/IssueNotFoundException.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Issues;
 
+use App\Shared\NotFoundException;
+
 final class IssueNotFoundException extends NotFoundException
 {
     protected static function getModelFCQN(): string
--- src/Shared/NotFoundException.php
+++ src/Shared/NotFoundException.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Issues;
+namespace App\Shared;
 
 use Ramsey\Uuid\UuidInterface;
 use RuntimeException;

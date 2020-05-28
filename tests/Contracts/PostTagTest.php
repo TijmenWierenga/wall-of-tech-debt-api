64e50ef9c8e50d3fb6ad5114bbe6b9bcd0d22be5
Tijmen Wierenga <tijmen.wierenga@persgroep.net>
2020-05-28 20:28:26 +0200

    Add authentication to contract test

--- tests/Contracts/PostTagTest.php
+++ tests/Contracts/PostTagTest.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace App\Tests\Contracts;
 
+use App\Domain\Security\TokenService;
+use App\Domain\Security\User;
+use App\Domain\Security\UserRepository;
 use App\Tests\DatabasePrimer;
 use Doctrine\ORM\EntityManagerInterface;
 use League\OpenAPIValidation\PSR7\OperationAddress;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 final class PostTagTest extends ContractTestCase
 {
@@ -21,12 +26,24 @@ final class PostTagTest extends ContractTestCase
 
     public function testItCreatesANewTag(): void
     {
+        $userRepository = static::$container->get(UserRepository::class);
+        $passwordEncoder = static::$container->get(UserPasswordEncoderInterface::class);
+        $user = User::new(Uuid::uuid4(), 'tijmen', '');
+        $user->setPassword($passwordEncoder->encodePassword($user, '123456'));
+        $userRepository->save($user);
+
+        $tokenService = static::$container->get(TokenService::class);
+        $token = $tokenService->createToken($user);
+
         $this->client->request(
             'POST',
             'tags',
             [],
             [],
-            ['CONTENT_TYPE' => 'application/json'],
+            [
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_AUTHORIZATION' => "Bearer ${token}"
+            ],
             json_encode([
                 'name' => 'docs'
             ], JSON_THROW_ON_ERROR)

1fa5477a0722edcf0213209b692b9792db03da23
Tijmen Wierenga <tijmen.wierenga@persgroep.net>
2020-05-28 08:13:09 +0200

    Created implementations for the tag repository

---
+++ src/Domain/Issues/TagRepositoryDoctrine.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Issues;
+
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\UuidInterface;
+
+final class TagRepositoryDoctrine implements TagRepository
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function save(Tag $tag): void
+    {
+        try {
+            $this->entityManager->persist($tag);
+            $this->entityManager->flush();
+        } catch (UniqueConstraintViolationException $e) {
+            throw TagAlreadyExistsException::withName($tag->name);
+        }
+    }
+
+    public function find(UuidInterface $tagId): Tag
+    {
+        $repository = $this->entityManager->getRepository(Tag::class);
+
+        $tag = $repository->find($tagId);
+
+        if (! $tag) {
+            throw TagNotFoundException::withId($tagId);
+        }
+
+        return $tag;
+    }
+}
---
+++ src/Domain/Issues/TagRepositoryInMemory.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Issues;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Ramsey\Uuid\UuidInterface;
+
+final class TagRepositoryInMemory implements TagRepository
+{
+    /** @var Collection<array-key,Tag> */
+    public Collection $tags;
+
+    public function __construct()
+    {
+        $this->tags = new ArrayCollection();
+    }
+
+    public function save(Tag $tag): void
+    {
+        $result = $this->tags->filter(fn (Tag $item): bool => $tag->name === $item->name);
+
+        if (! $result->isEmpty()) {
+            throw TagAlreadyExistsException::withName($tag->name);
+        }
+
+        $this->tags->add($tag);
+    }
+
+    public function find(UuidInterface $tagId): Tag
+    {
+        $result = $this->tags
+            ->filter(fn (Tag $item): bool => $tagId->toString() === $item->getId()->toString())
+            ->first();
+
+        if (! $result) {
+            throw TagNotFoundException::withId($tagId);
+        }
+
+        return $result;
+    }
+}

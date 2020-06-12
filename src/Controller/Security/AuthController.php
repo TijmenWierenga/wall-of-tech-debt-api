ab4b69a00f6656adc3a6178d43ba22fd36928e3f
TijmenWierenga <t.wierenga@live.nl>
2020-06-12 16:07:17 +0200

    Created endpoint for fetching the currently authenticated user

--- src/Controller/Security/AuthController.php
+++ src/Controller/Security/AuthController.php
@@ -5,17 +5,21 @@ declare(strict_types=1);
 namespace App\Controller\Security;
 
 use App\Domain\Security\LoginService;
+use App\Domain\Security\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 final class AuthController
 {
     private LoginService $loginService;
+    private Security $security;
 
-    public function __construct(LoginService $loginService)
+    public function __construct(LoginService $loginService, Security $security)
     {
         $this->loginService = $loginService;
+        $this->security = $security;
     }
 
     /**
@@ -29,6 +33,22 @@ final class AuthController
 
         return new JsonResponse([
             'token' => $token
+        ]);
+    }
+
+    /**
+     * @Route("/me", methods={"GET"})
+     */
+    public function getAuthenticatedUser(): JsonResponse
+    {
+        /** @var User $user */
+        $user = $this->security->getUser();
+
+        return new JsonResponse([
+            'id' => $user->getId()->toString(),
+            'username' => $user->getUsername(),
+            'firstName' => $user->getFirstName(),
+            'lastName' => $user->getLastName()
         ]);
     }
 }

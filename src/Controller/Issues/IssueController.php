cbde3cfae07b2c6a9cbe024ab57697d1bc307239
TijmenWierenga <t.wierenga@live.nl>
2020-07-06 21:04:11 +0200

    Use Fractal for transforming entities

--- src/Controller/Issues/IssueController.php
+++ src/Controller/Issues/IssueController.php
@@ -6,9 +6,13 @@ namespace App\Controller\Issues;
 
 use App\Domain\Issues\Issue;
 use App\Domain\Issues\IssueService;
+use App\Domain\Issues\IssueTransformer;
 use App\Domain\Issues\Tag;
 use App\Domain\Issues\Vote;
 use App\Domain\Security\User;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,14 +22,15 @@ use Symfony\Component\Security\Core\Security;
 
 final class IssueController
 {
-    /** @var IssueService */
     private IssueService $issueService;
     private Security $security;
+    private Manager $transformer;
 
-    public function __construct(IssueService $issueService, Security $security)
+    public function __construct(IssueService $issueService, Security $security, Manager $transformer)
     {
         $this->issueService = $issueService;
         $this->security = $security;
+        $this->transformer = $transformer;
     }
 
     /**
@@ -33,28 +38,17 @@ final class IssueController
      */
     public function index(): Response
     {
-        $issues = $this->issueService->getAll()->map(fn (Issue $issue): array => [
-            'id' => $issue->getId()->toString(),
-            'title' => $issue->getTitle(),
-            'createdAt' => $issue->getCreatedAt()->format(DATE_ATOM),
-            'author' => $issue->getAuthorId()->toString(),
-            'votes' => $issue->getVotes()->map(fn (Vote $vote): array => [
-                'by' => $vote->getUserId()->toString(),
-                'amount' => $vote->getAmount()
-            ])->toArray(),
-            'tags' => $issue->getTags()->map(fn (Tag $tag): array => [
-                'id' => $tag->getId(),
-                'name' => $tag->name
-            ])->toArray()
-        ])->toArray();
+        $issues = $this->issueService->getAll();
 
-        return new JsonResponse($issues);
+        $data = $this->transformer->createData(new Collection($issues, new IssueTransformer()));
+
+        return new JsonResponse($data->toArray());
     }
 
     /**
      * @Route("/issues", methods={"POST"})
      */
-    public function create(Request $request): Response
+    public function create(Request $request): JsonResponse
     {
         /** @var User $user */
         $user = $this->security->getUser();
@@ -62,12 +56,14 @@ final class IssueController
 
         $data = json_decode((string)$request->getContent(), true, 512, JSON_THROW_ON_ERROR);
 
-        $this->issueService->create(
+        $issue = $this->issueService->create(
             $data['title'],
             $userId
         );
 
-        return new Response(null, Response::HTTP_CREATED, [
+        $data = $this->transformer->createData(new Item($issue, new IssueTransformer()));
+
+        return new JsonResponse($data->toArray(), Response::HTTP_CREATED, [
             'Content-Type' => 'application/json'
         ]);
     }
